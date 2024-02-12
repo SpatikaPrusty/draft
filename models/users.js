7a6310aeff332031ed997e338d7bbd8476071710
SpatikaPrusty <kspatika.prusty@lumiq.ai>
2024-02-12 10:40:18 +0530

    Creating models

--- models/users.js
+++ models/users.js
@@ -1,15 +1,25 @@
 const mongoose = require('mongoose');
 
-const { schema } = mongoose;
+const userSchema = mongoose.Schema(
+    {
+        name:{type: String, required: [true,'Please enter your name.']},
+        age:{type: Number, required: [true,'Please enter your age.']},
+        isSmoke:{type: Boolean, required: [true]},
+        isDiabetic:{type: Boolean, required: [true]},
+        incomePerAnnum: {type: Number, default: 0}
+    },
+    {
+        timestamps: true
+    }
+);
 
+// const users = new Schema({
+//     name: { type: String },
+//     age: { type: Number },
+//     isSmoke: { type: Boolean },
+//     isDiabetic: { type: Boolean },
+//     incomePerAnnum: {type: Number, default: 0}
+// });
 
-const users = new schema({
-    name: { type: String },
-    age: { type: Number },
-    isSmoke: { type: Boolean },
-    isDiabetic: { type: Boolean },
-    incomePerAnnum: {type: Number, default: 0}
-});
-
-const User = mongoose.model('User', users);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
